4bc4c178c7dbad6c3baa9e57c571d37351c18c2c
informatics411 <informatics411@gmail.com>
2023-04-16 22:25:08 -0400

    authorize NoteController

---
+++ ElevenNote.WebAPI/Controllers/NoteController.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Authorization;
+
+[Authorize]
+[ApiController]
+[Route("api/[controller]")]
+public class NoteController : ControllerBase
+{
+    private readonly INoteService _noteService;
+    public NoteController(INoteService noteService)
+    {
+        _noteService = noteService;
+    }
+}
+
